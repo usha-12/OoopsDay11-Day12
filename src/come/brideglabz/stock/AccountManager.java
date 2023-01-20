1281d8991b08676b32b1f116d7273ccca5854a07
usha-12 <usha12ahirwar@gmail.com>
2023-01-20 17:39:02 +0530

    Added: Debit withdraw from account

--- src/come/brideglabz/stock/AccountManager.java
+++ src/come/brideglabz/stock/AccountManager.java
@@ -1,2 +1,10 @@
-package come.brideglabz.objectorientedprograms;public class AccountManager {
+package come.brideglabz.stock;
+
+public class AccountManager {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Stock Account Management Program!");
+
+        StockPortfolio portfolio = new StockPortfolio();
+        portfolio.selectActivity();
+    }
 }
